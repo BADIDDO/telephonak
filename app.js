52a2f5a851fabc92bdb90dc521e34091a995c382
BADIDDO <aymenmatrix311@gmail.com>
2022-01-18 22:16:10 +0100

    update

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".three-bars");
+const links = document.querySelector(".links");
+
+
+btn.addEventListener("click" , () => {
+    links.style.display = "block" ;
+})
